c928a6d0faae5bce5f90d8136f98a9810d9ff416
AndrewXing43 <139531626+AndrewXing43@users.noreply.github.com>
2025-04-30 16:31:37 -0700

    Update train.py

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader, random_split
-from dataset1 import RadioMapDataset
+from dataset import RadioMapDataset
 from model import UNet
 from utils import MSELoss, compute_rmse, save_checkpoint, custom_collate_fn, plot_loss_curve
 from pathlib import Path
